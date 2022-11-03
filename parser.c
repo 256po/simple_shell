3765bd131bda08c4af759f6ffa072eae277a1abd
256po <paul.bewayo4@gmail.com>
2022-11-03 06:35:04 -0700

    God is Able

--- parser.c
+++ parser.c
@@ -35,8 +35,8 @@ char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
 	int i = 0, k = 0;
-	for (k = 0, i = start; i < stop; i++)
 
+	for (k = 0, i = start; i < stop; i++)
 		if (pathstr[i] != ':')
 			buf[k++] = pathstr[i];
 	buf[k] = 0;
@@ -56,8 +56,8 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 {
 	int i = 0, curr_pos = 0;
 	char *path;
-	if (!pathstr)
 
+	if (!pathstr)
 		return (NULL);
 	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
 	{
